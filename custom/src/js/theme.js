3235acb21e0e577700fb758542b9a2b3f2cbe0fc
Omar Vásquez <omarvs91@gmail.com>
2023-09-11 03:47:45 -0500

    Adding laravel mix

---
+++ custom/src/js/theme.js
@@ -0,0 +1,196 @@
+(function( $ ) {
+    "use strict";
+
+    // Script for whatsapp and fb messenger widget icon
+    $(document).ready(function() {
+        var zaloWidgetInterval;
+        var tawkToInterval;
+        var skypeWidgetInterval;
+        var lcpWidgetInterval;
+        var closePopupTimeout;
+        var lzWidgetInterval;
+        var arcuOptions;
+        var arCuMessages = ["\u00a1Hola!", "\u00bfTienes alguna pregunta?", "Por favor use este bot\u00f3n<br \/>\n\u00a1para contáctarnos!"];
+        var arCuLoop = false;
+        var arCuCloseLastMessage = false;
+        var arCuPromptClosed = false;
+        var _arCuTimeOut = null;
+        var arCuDelayFirst = 2000;
+        var arCuTypingTime = 2000;
+        var arCuMessageTime = 4000;
+        var arCuClosedCookie = 0;
+        var arcItems = [];
+        window.addEventListener('load', function () {
+            jQuery('#arcontactus').remove();
+            var $arcuWidget = jQuery('<div>', {
+                id: 'arcontactus'
+            });
+            jQuery('body').append($arcuWidget);
+            arCuClosedCookie = arCuGetCookie('arcu-closed');
+            jQuery('#arcontactus').on('arcontactus.init', function () {
+                if (arCuClosedCookie) {
+                    return false;
+                }
+                arCuShowMessages();
+            });
+            jQuery('#arcontactus').on('arcontactus.closeMenu', function () {
+                arCuCreateCookie('arcumenu-closed', 1, 1);
+            });
+            jQuery('#arcontactus').on('arcontactus.openMenu', function () {
+                clearTimeout(_arCuTimeOut);
+                if (!arCuPromptClosed) {
+                    arCuPromptClosed = true;
+                    jQuery('#arcontactus').contactUs('hidePrompt');
+                }
+            });
+            jQuery('#arcontactus').on('arcontactus.openCallbackPopup', function () {
+                clearTimeout(_arCuTimeOut);
+                if (!arCuPromptClosed) {
+                    arCuPromptClosed = true;
+                    jQuery('#arcontactus').contactUs('hidePrompt');
+                }
+            });
+
+            jQuery('#arcontactus').on('arcontactus.hidePrompt', function () {
+                clearTimeout(_arCuTimeOut);
+                if (arCuClosedCookie != "1") {
+                    arCuClosedCookie = "1";
+                    arCuPromptClosed = true;
+                    arCuCreateCookie('arcu-closed', 1, 0);
+                }
+            });
+            var arcItem = {};
+            arcItem.id = 'msg-item-2';
+            arcItem.class = 'msg-item-whatsapp';
+            arcItem.title = "WhatsApp";
+            arcItem.icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path></svg>';
+            arcItem.href = 'https://wa.me/51945325508';
+            arcItem.color = '#00BB2D';
+            arcItems.push(arcItem);
+            var arcItem = {};
+            arcItem.id = 'msg-item-1';
+            arcItem.class = 'msg-item-facebook-messenger';
+            arcItem.title = "Messenger";
+            arcItem.icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 32C15.9 32-77.5 278 84.6 400.6V480l75.7-42c142.2 39.8 285.4-59.9 285.4-198.7C445.8 124.8 346.5 32 224 32zm23.4 278.1L190 250.5 79.6 311.6l121.1-128.5 57.4 59.6 110.4-61.1-121.1 128.5z"></path></svg>';
+            arcItem.href = 'https://www.facebook.com/messages/t/107972727266902';
+            arcItem.color = '#567AFF';
+            arcItems.push(arcItem);
+
+            arcuOptions = {
+                wordpressPluginVersion: '1.7.9',
+                buttonIcon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path></svg>',
+                drag: false,
+                mode: 'regular',
+                buttonIconUrl: 'assets/fonts/msg.svg',
+                showMenuHeader: false,
+                menuHeaderText: "How would you like to contact us?",
+                showHeaderCloseBtn: false,
+                headerCloseBtnBgColor: '#008749',
+                headerCloseBtnColor: '#ffffff',
+                itemsIconType: 'rounded',
+                align: 'right',
+                reCaptcha: false,
+                reCaptchaKey: '',
+                countdown: 0,
+                theme: '#00bb2d',
+                buttonText: "Chat",
+                buttonSize: 'medium',
+                menuSize: 'large',
+                phonePlaceholder: '+XXX-XX-XXX-XX-XX',
+                callbackSubmitText: 'Solicitar llamada',
+                errorMessage: 'Error de conexión. Actualice la página e intente nuevamente.',
+                callProcessText: 'Te estamos llamando al telefono',
+                callSuccessText: 'Gracias.<br />Le devolveremos la llamada pronto.',
+                iconsAnimationSpeed: 600,
+                iconsAnimationPause: 2000,
+                callbackFormText: 'Por favor, introduzca su número de teléfono<br />y te llamamos pronto',
+                items: arcItems,
+                ajaxUrl: '#',
+                promptPosition: 'top',
+                callbackFormFields: {
+                    phone: {
+                        name: 'phone',
+                        enabled: true,
+                        required: true,
+                        type: 'tel',
+                        label: '',
+                        placeholder: "+XX-XXX-XXX-XXX"
+                    },
+                },
+                action: 'arcontactus_request_callback'
+            };
+            jQuery('#arcontactus').contactUs(arcuOptions);
+        });
+        function arCuGetCookie(cookieName){
+            if (document.cookie.length > 0) {
+                c_start = document.cookie.indexOf(cookieName + "=");
+                if (c_start != -1) {
+                    c_start = c_start + cookieName.length + 1;
+                    c_end = document.cookie.indexOf(";", c_start);
+                    if (c_end == -1) {
+                        c_end = document.cookie.length;
+                    }
+                    return unescape(document.cookie.substring(c_start, c_end));
+                }
+            }
+            return 0;
+        };
+        function arCuCreateCookie(name, value, days){
+            var expires;
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toGMTString();
+            } else {
+                expires = "";
+            }
+            document.cookie = name + "=" + value + expires + "; path=/";
+        };
+        function arCuShowMessage(index){
+            if (arCuPromptClosed){
+                return false;
+            }
+            if (typeof arCuMessages[index] !== 'undefined'){
+                jQuery('#arcontactus').contactUs('showPromptTyping');
+        
+                _arCuTimeOut = setTimeout(function(){
+                    if (arCuPromptClosed){
+                        return false;
+                    }
+                    jQuery('#arcontactus').contactUs('showPrompt', {
+                        content: arCuMessages[index]
+                    });
+                    index ++;
+                    _arCuTimeOut = setTimeout(function(){
+                        if (arCuPromptClosed){
+                            return false;
+                        }
+                        arCuShowMessage(index);
+                    }, arCuMessageTime);
+                }, arCuTypingTime);
+            }else{
+                if (arCuCloseLastMessage){
+                    jQuery('#arcontactus').contactUs('hidePrompt');
+                }
+                if (arCuLoop){
+                    arCuShowMessage(0);
+                }
+            }
+        };
+        function arCuShowMessages(){
+            setTimeout(function(){
+                clearTimeout(_arCuTimeOut);
+                arCuShowMessage(0);
+            }, arCuDelayFirst);
+        }
+        window.addEventListener('load', function(){
+            jQuery('#arcontactus-storefront-btn').click(function(e){
+                e.preventDefault();
+                setTimeout(function(){
+                    jQuery('#arcontactus').contactUs('openMenu');
+                }, 200);
+            });
+        });
+    });
+
+})(jQuery);
