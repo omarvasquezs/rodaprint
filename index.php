abf54f142ccfffcbf87e9d8b919e675980b686eb
Omar Vásquez <omarvs91@gmail.com>
2023-09-16 13:16:02 -0500

    Getting rid of more not used files and libraries and migrating more styles to the custom theme, changing some folders to improve the folder structure of the project

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RodaPrint Comunicando Ideas...</title>    
+    <!-- FAVICON ICONS -->
+    <link rel="icon" href="custom/src/images/cropped-roda-32x32.png" sizes="32x32">
+    <link rel="icon" href="custom/src/images/cropped-roda-192x192.png" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="custom/src/images/cropped-roda-180x180.png">
+    <meta name="msapplication-TileImage" content="custom/src/images/cropped-roda-270x270.png">
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Main Style -->
+    <link href="style.css" rel="stylesheet">
+    <!-- STYLES FOR CHAT WIDGET (DO NOT REMOVE) -->
+    <link rel='stylesheet' id='jquery.contactus.css-css' href='assets/css/jquery.contactus.min97e9.css?ver=1.7.9' type='text/css' media='all' />
+    <!-- CUSTOM THEME CSS -->
+    <link href="themes/rodaprint_custom/style.css" rel="stylesheet">    
+</head>
+<body>
+    <!-- START CHAT WIDGET -->
+    <div id="arcontactus"></div>
+    <!-- END CHAT WIDGET -->
+    <!-- Start Header -->
+    <header id="mu-header" class="" role="banner">
+        <div class="container">
+            <nav class="navbar navbar-default mu-navbar">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <!-- Image Logo -->
+                        <a class="navbar-brand" style="max-width: 130px;" href="/"><img src="assets/images/logo.jpg"></a>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav mu-menu front-nav-bar">
+                            <li><a href="#mu-home">NOSOTROS</a></li>
+                            <li><a href="">LINEAS DE NEGOCIO</a></li>
+                            <li><a href="">CLIENTES</a></li>
+                            <li><a href="">CONTÁCTANOS</a></li>
+                        </ul>
+                        <nav class="telephone-container text-center">
+                            <a href="tel:51945325508"><span class="llamanos">LLÁMANOS AL</span><br><span class="llamanos-numero">945-325-508</span></a>
+                        </nav>                        
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </div>
+    </header>
+    <!-- End Header -->
+    <!--    INICIO DE VIDEO ---->
+    <section id="mu-counter" style="margin-bottom: 5rem;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="mu-featured-slide front-video">
+                    <video controls loop autoplay muted name="media">
+                        <source src="assets/videos/rodaprint.mp4" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- FIN DE VIDEO --->
+    <!-- Start main content -->
+    <main role="main">
+        <!-- Start About -->
+        <section id="mu-home">
+            <div style="background-color: #fff;">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mu-about-area">
+                                <div style="margin-bottom: 5rem;" class="text-center">
+                                    <img src="assets/images/logo.jpg">
+                                </div>
+                                <div class="mu-about-header peruoffset-texto">                                    
+                                    <h2>Quiénes Somos?</h2>
+                                    <p style="color:#5D686E;">En Rodaprint nuestro compromiso a la innovación y al excelente servicio es respaldado por nuestro equipo profesional. Siempre manteniéndonos a la vanguardia de la industria en calidad total, diversidad de servicios, y cuidado del medio ambiente.</p>
+                                    <p style="color: #5D686E">Nuestra trayectoria nos permite ofrecer diversidad de productos impresos con la mayor calidad en el menor tiempo posible y sin impacto al medio ambiente. Porque creemos firmemente que el buen trabajo es el bien hecho haciendo 10 correcto.</p>
+                                    <p style="color: #5D686E;margin-bottom:5rem;">Es gracias a nuestros clientes estamos rumbo a ser una empresa lider a nivel nacional.</p>
+                                    <h2>Misión</h2>
+                                    <p style="color:#5D686E;margin-bottom:5rem;">Brindar soluciones graficas e integrales, enfocándonos en la impresión y la implementación de elementos gráficos adaptándonos a sus necesidades de los clientes.</p>
+                                    <h2>Vision</h2>
+                                    <p style="color:#5D686E;margin-bottom:5rem;">Convertirnos en la primera opción en soluciones gráficas de los requerimientos de impresión e implementación de las principales industrias del país.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End About -->
+        
+        <!-- Start Google Map -->
+        <section id="mu-google-map" style="margin-bottom: 5rem;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3901.5727809820823!2d-77.0034776!3d-12.0728883!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c625f5c8cdf3%3A0x3385f382168f0bf6!2sRodaPrint!5e0!3m2!1ses!2spe!4v1694834066377!5m2!1ses!2spe" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </section>
+        <!-- End Google Map -->
+    </main>
+    <!-- End main content -->
+    <!-- Start footer -->
+    <footer class="footer" style="margin-bottom: 4rem;">
+        <div class="container">
+            <div class="row">
+                <!-- Column 1: Social Icons -->
+                <div class="col-md-4 col-sm-12 text-center" style="margin-bottom: 4rem;">
+                    <h4 style="margin-bottom: 2rem;">SIGUENOS EN:</h4>
+                    <div class="d-flex justify-content-center">
+                        <!-- Add your social icons here, e.g., Font Awesome icons -->
+                        <a style="margin-right: 15px;" href="https://www.facebook.com/RodaPrintPeru" target="_blank"><img src="assets/images/facebook_icon.png" alt="Facebook"></a>
+                        <a style="margin-right: 15px;" href="https://www.instagram.com/rodaprint" target="_blank><img src="assets/images/instagram_icon.png" alt="Instagram""></a>
+                    </div>
+                </div>
+                
+                <!-- Column 2: Logo -->
+                <div class="col-md-4 col-sm-12 text-center" style="margin-bottom: 4rem;">
+                    <!-- Replace the src attribute with your logo image URL -->
+                    <img src="assets/images/logo.jpg" alt="Logo" class="img-responsive center-block">
+                </div>
+                
+                <!-- Column 3: Address -->
+                <div class="col-md-4 col-sm-12 text-center" style="margin-bottom: 4rem;">
+                    <h4>CONTACTANOS</h4>
+                    <p>RodaPrint S.A.</p>
+                    <p>Av Nicolas Arriola #2000 Int 15, San Luis</p>
+                    <p>Lima, Peru</p>
+                    <p>Correo: <a href="mailto:gerencia@rodaprint.com.pe">gerencia@rodaprint.com.pe</a></p>
+                    <p>Teléfono: <a href="tel:51945325508">945 325 508</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer -->
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- JQUERY -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <!-- CHAT WIDGET JS -->
+    <script src="assets/js/jquery.contactus.mine606.js?version=1.7.9" defer></script>
+    <!-- Bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Custom js -->    
+    <script type="text/javascript" src="themes/rodaprint_custom/theme.js"></script>    
+</body>
+</html>
