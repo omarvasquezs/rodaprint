1a5ee8475af268d0eb262f0e9c6e2fbd8346dfe7
Omar Vásquez <omarvs91@gmail.com>
2023-09-18 03:35:49 -0500

    Adding layouts and other pages

---
+++ layouts/footer.php
@@ -0,0 +1,51 @@
+    <!-- Start Google Map -->
+    <div class="container-fluid mb-5">
+        <div class="row">
+            <div class="col-12 px-0">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3901.5727809820823!2d-77.0034776!3d-12.0728883!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c625f5c8cdf3%3A0x3385f382168f0bf6!2sRodaPrint!5e0!3m2!1ses!2spe!4v1694834066377!5m2!1ses!2spe" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+    <!-- End Google Map -->
+    <!-- Start footer -->
+    <footer class="footer">
+        <div class="container mb-5">
+            <div class="row">
+                <!-- Column 1: Social Icons -->
+                <div class="col-md-4 col-sm-12 mb-4 text-center">
+                    <h4>SIGUENOS EN:</h4>
+                    <div class="d-flex justify-content-center">
+                        <!-- Add your social icons here, e.g., Font Awesome icons -->
+                        <a class="pe-4" href="https://www.facebook.com/RodaPrintPeru" target="_blank"><img src="assets/images/facebook_icon.png" alt="Facebook"></a>
+                        <a href="https://www.instagram.com/rodaprint" target="_blank"><img src="assets/images/instagram_icon.png" alt="Instagram"></a>
+                    </div>
+                </div>                
+                <!-- Column 2: Logo -->
+                <div class="col-md-4 col-sm-12 mb-4 text-center order-3 order-md-2">
+                    <!-- Replace the src attribute with your logo image URL -->
+                    <img src="assets/images/logo.jpg" alt="Logo" class="img-responsive center-block">
+                </div>                
+                <!-- Column 3: Address -->
+                <div class="col-md-4 col-sm-12 mb-4 text-center order-2 order-md-3">
+                    <h4>CONTACTANOS</h4>
+                    <p>RodaPrint S.A.</p>
+                    <p>Av Nicolas Arriola #2000 Int 15, San Luis</p>
+                    <p>Lima, Peru</p>
+                    <p>Correo: <a href="mailto:gerencia@rodaprint.com.pe">gerencia@rodaprint.com.pe</a></p>
+                    <p>Teléfono: <a href="tel:51945325508">945 325 508</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer -->
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- JQUERY -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <!-- CHAT WIDGET JS -->
+    <script src="libraries/contactus/js/jquery.contactus.mine606.js?version=1.7.9" defer></script>
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- Custom js -->    
+    <script type="text/javascript" src="themes/rodaprint_custom/theme.js"></script>    
+</body>
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RodaPrint "Comunicando Ideas"</title>    
+    <!-- FAVICON ICONS -->
+    <link rel="icon" href="assets/images/cropped-roda-32x32.png" sizes="32x32">
+    <link rel="icon" href="assets/images/cropped-roda-192x192.png" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="assets/images/cropped-roda-180x180.png">
+    <meta name="msapplication-TileImage" content="assets/images/cropped-roda-270x270.png">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- STYLES FOR CHAT WIDGET (DO NOT REMOVE) -->
+    <link rel='stylesheet' id='jquery.contactus.css-css' href='libraries/contactus/css/jquery.contactus.min97e9.css?ver=1.7.9' type='text/css' media='all' />
+    <!-- CUSTOM THEME CSS -->
+    <link href="themes/rodaprint_custom/style.css" rel="stylesheet">    
+</head>
+<body>
+    <!-- START CHAT WIDGET -->
+    <div id="arcontactus"></div>
+    <!-- END CHAT WIDGET -->
+    <!-- Start Header (Navbar) -->
+    <header>
+        <nav class="navbar navbar-custom navbar-expand-lg navbar-light bg-white">
+            <div class="container">
+                <a class="navbar-brand mx-auto ps-5" href="/">
+                <img src="assets/images/logo.jpg" alt="Logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav custom-menu me-auto mb-2 mb-lg-0 mx-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="/#nosotros">NOSOTROS</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        LÍNEAS DE NEGOCIO
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="offset.php">OFFSET</a></li>
+                        <li><a class="dropdown-item" href="visual.php">VISUAL - GRAN FORMATO</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">CLIENTES</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">CONTÁCTANOS</a>
+                    </li>
+                </ul>
+                <span class="telephone-container text-center">
+                    <a href="tel:51945325508"><span class="llamanos">LLÁMANOS AL</span><br><span class="llamanos-numero">945-325-508</span></a>
+                </span>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- End Header -->
