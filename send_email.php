2b9945025d8fa1855bc30de97073410a2e34c493
Omar Vásquez <omarvs91@gmail.com>
2023-09-24 17:01:29 -0500

    Updating send_email.php so no one can send emails by accessing it through url

--- send_email.php
+++ send_email.php
@@ -1,37 +1,44 @@
 <?php
 require 'vendor/autoload.php'; // Include the Mailjet PHP library
 
-// Replace with your Mailjet API credentials
-$api_key = '59ca8a9d022970993dd3b76473414c41';
-$api_secret = '98dcfc4c85797c0c4f8f09c5bd84dd07';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // This block will only execute if the request is a POST request, meaning it's from a form submission.
 
-// Initialize the Mailjet client
-$mj = new \Mailjet\Client($api_key, $api_secret);
+    // Replace with your Mailjet API credentials
+    $api_key = '59ca8a9d022970993dd3b76473414c41';
+    $api_secret = '98dcfc4c85797c0c4f8f09c5bd84dd07';
 
-// Get form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$telephone = $_POST['telephone'];
-$message = $_POST['message'];
+    // Initialize the Mailjet client
+    $mj = new \Mailjet\Client($api_key, $api_secret);
 
-// Create an email message
-$emailData = [
-    'FromEmail' => 'notificaciones@rodaprint.com.pe',
-    'FromName' => 'Notificaciones Rodaprint',
-    'Subject' => $_POST['email'].' quiere contactarse',
-    'Text-part' => "Name: $name\nEmail: $email\nTelephone: $telephone\nMessage: $message",
-    'Recipients' => [['Email' => 'gerencia@rodaprint.com.pe']], // Your email address
-];
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $message = $_POST['message'];
 
-// Send the email
-$response = $mj->post(\Mailjet\Resources::$Email, ['body' => $emailData]);
+    // Create an email message
+    $emailData = [
+        'FromEmail' => 'notificaciones@rodaprint.com.pe',
+        'FromName' => 'Notificaciones Rodaprint',
+        'Subject' => $_POST['email'].' quiere contactarse',
+        'Text-part' => "Nombres: $name\nCorreo: $email\nTelefono: $telephone\nMensaje: $message",
+        'Recipients' => [['Email' => 'omarvs91@gmail.com']], // Your email address
+    ];
 
-if ($response->success()) {
-    echo '<center><h1>Correo enviado con éxito!</h1><br><p>Sera redireccionado en breves momentos...</p></center>';
-    header("Refresh: 2; url=/");
-    exit;
+    // Send the email
+    $response = $mj->post(\Mailjet\Resources::$Email, ['body' => $emailData]);
+
+    if ($response->success()) {
+        echo '<center><h1>Correo enviado con éxito!</h1><br><p>Sera redireccionado en breves momentos...</p></center>';
+        header("Refresh: 2; url=/");
+        exit;
+    } else {
+        echo 'Email sending failed: ' . $response->getStatus() . ' - ' . $response->getBody();
+        header("Refresh: 2; url=/");
+        exit;
+    }
 } else {
-    echo 'Email sending failed: ' . $response->getStatus() . ' - ' . $response->getBody();
-    header("Refresh: 2; url=/");
-    exit;
+    // If the request is not a POST request (e.g., accessed via URL), you can handle it accordingly.
+    echo 'Invalid request.';
 }
